e001e2ff10d09b726821da8f17921f7cb68c065d
Pavithran <pavithrank02@gmail.com>
2022-10-17 07:50:52 +0530

    updated error occurence

---
+++ src/components/MainHeader.js
@@ -0,0 +1,15 @@
+const MainHeader = () => {
+return <header>
+    <nav>
+        <ul>
+            <li>
+                <a href='/welcome'>Welcome</a>
+            </li>
+            <li>
+                <a href='/products'>Welcome</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+}
+export default MainHeader;
