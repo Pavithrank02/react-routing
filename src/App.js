d29f10fc9d1369f7610bc7c5107c5db378e9d285
Pavithran <pavithrank02@gmail.com>
2022-10-17 08:28:35 +0530

    dynamic path setting for traversing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetail';
 import MainHeader from './components/MainHeader';
 
 function App() {
@@ -15,6 +16,9 @@ function App() {
     </Route>
     <Route path="/products">
       <Products />
+    </Route>
+    <Route path="/product-details/:productId">
+      <ProductDetails />
     </Route>
 
     </main>
---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,7 @@
+const ProductDetails = () => {
+return <section>
+    <h1>Product Details</h1>
+</section>
+}
+
+export default ProductDetails;
