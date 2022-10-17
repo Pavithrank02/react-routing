4bed609671c65aacdc8e77faded74f50179d79a9
Pavithran <pavithrank02@gmail.com>
2022-10-17 07:57:05 +0530

    utilizng links for navigation

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom";
+
 const MainHeader = () => {
 return <header>
     <nav>
         <ul>
             <li>
-                <a href='/welcome'>Welcome</a>
+                <Link to='/welcome'>Welcome </Link>
             </li>
             <li>
-                <a href='/products'>Welcome</a>
+                <Link to='/products'>Products</Link>
             </li>
         </ul>
-    </nav>
+    </nav> 
 </header>
 }
 export default MainHeader;
